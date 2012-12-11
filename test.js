64e2d1c7abffb6395d29010709ed3145b5355093
Kevin Whitman <keverw92@gmail.com>
2012-12-11 06:02:22 -0500

    renamed db_revolver to just revolver

--- test.js
+++ test.js
@@ -1,82 +1,82 @@
-var db_revolver = require('./revolver.js');
+var revolver = require('./revolver.js');
 
 //add bullets
-console.log(db_revolver.add('Hai'));
-console.log(db_revolver.add('OMG'));
-console.log(db_revolver.add('LMAO'));
+console.log(revolver.add('Hai'));
+console.log(revolver.add('OMG'));
+console.log(revolver.add('LMAO'));
 
-//db_revolver.remove(1);
+//revolver.remove(1);
 
 //fire
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.add('HI guys');
+revolver.add('HI guys');
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
--- test2.js
+++ test2.js
@@ -1,45 +1,45 @@
-var db_revolver = require('./revolver.js');
+var revolver = require('./revolver.js');
 
 //add bullets
-console.log(db_revolver.add('Hai'));
-console.log(db_revolver.add('OMG'));
-console.log(db_revolver.add('LMAO'));
+console.log(revolver.add('Hai'));
+console.log(revolver.add('OMG'));
+console.log(revolver.add('LMAO'));
 
-db_revolver.remove(1);
+revolver.remove(1);
 
-console.log(db_revolver.removedIDs);
+console.log(revolver.removedIDs);
 
-console.log(db_revolver.add('Hai #2', 1));
+console.log(revolver.add('Hai #2', 1));
 
-console.log(db_revolver.removedIDs);
+console.log(revolver.removedIDs);
 
 //fire
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
 
-db_revolver.fire(function(id, bullet)
+revolver.fire(function(id, bullet)
 {
 	console.log(id, bullet);
 });
